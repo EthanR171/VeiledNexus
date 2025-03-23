bb72c6780334f16ecebebe4ccf5eb2a9fb53bfca
Ethan Rivers <143448114+EthanR171@users.noreply.github.com>
2025-03-23 14:27:30 -0400

    Added image to login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Paper, CardHeader, CardContent, TextField, Button, Alert } from '@mui/material';
 
+import logo from '../assets/cthulhu.jpg';
+
 const Login = (props) => {
   const [roomName, setRoomName] = useState('');
   const [userName, setUserName] = useState('');
@@ -8,6 +10,7 @@ const Login = (props) => {
   return (
     <Paper elevation={4} sx={{ mt: '0.5em' }}>
       <CardContent>
+        <img style={{ width: '10%', maxWidth: '150px', maxHeight: '150px', borderRadius: '50%' }} src={logo} />
         <CardHeader title="Join a Room" />
         <TextField fullWidth label="User Name" value={userName} onChange={(e) => setUserName(e.target.value)} sx={{ mb: '1em' }} />
         <TextField fullWidth label="Room Name" value={roomName} onChange={(e) => setRoomName(e.target.value)} sx={{ mb: '1em' }} />
