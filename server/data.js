3321ac767718b31bcaee4d2106a81a91382b7d35
Ethan Rivers <143448114+EthanR171@users.noreply.github.com>
2025-04-03 15:16:48 -0400

    Refactored room object into a class

--- server/data.js
+++ server/data.js
@@ -15,13 +15,48 @@ const isUserNameTaken = (userName) => {
 };
 
 // Message Tracking
-let roomLogs = {};
-const roomLog = (room) => roomLogs[room];
-const addMessage = (room, messageInfo) => {
-  if (!roomLogs[room]) {
-    roomLogs[room] = [];
+
+class Room {
+  static #rooms = {};
+
+  static get(roomName) {
+    if (!Room.#rooms[roomName]) {
+      Room.#rooms[roomName] = new Room(roomName);
+    }
+    return this.#rooms[roomName];
   }
-  roomLogs[room].push(messageInfo);
+
+  #name = '';
+  #log = [];
+
+  /* Ctor */
+  constructor(name) {
+    this.#name = name;
+  }
+
+  /* Instance Methods */
+  addMessage(messageInfo) {
+    messageInfo.timestamp = Date.now();
+    this.#log.push(messageInfo);
+  }
+
+  /* Accessors */
+  get name() {
+    return this.#name;
+  }
+
+  get log() {
+    return this.#log;
+  }
+}
+
+/* Module methods */
+const roomLog = (roomName) => {
+  return Room.get(roomName).log;
+};
+
+const addMessage = (roomName, messageInfo) => {
+  Room.get(roomName).addMessage(messageInfo);
 };
 
 export { registerUser, unregisterUser, isUserNameTaken, roomLog, addMessage };
